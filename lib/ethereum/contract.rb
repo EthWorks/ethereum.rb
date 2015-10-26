1f290623467cf20c345ce85c6a735b794154b0b6
Anthony Eufemio <tymat@pris.im>
2015-10-27 00:37:02 +0900

    fixed bug with deployment parameters

--- lib/ethereum/contract.rb
+++ lib/ethereum/contract.rb
@@ -22,7 +22,7 @@ module Ethereum
       class_name = @name
       functions = @functions
       constructor_inputs = @constructor_inputs
-      code = @code
+      binary = @code
 
       class_methods = Class.new do
 
@@ -32,57 +32,61 @@ module Ethereum
 
         define_method :deploy do |*params|
           formatter = Ethereum::Formatter.new
+          deploy_code = binary
+          deploy_arguments = ""
           if constructor_inputs.present?
             raise "Missing constructor parameter" and return if params.length != constructor_inputs.length
             constructor_inputs.each_index do |i|
               args = [constructor_inputs[i]["type"], params[i]]
-              code << formatter.to_payload(args)
+              deploy_arguments << formatter.to_payload(args)
             end
           end
-          deploytx = connection.send_transaction({from: self.sender, gas: 2000000, gasPrice: 60000000000, data: code})["result"]
-          self.instance_variable_set("@deployment", Ethereum::Deployment.new(deploytx, connection))
+          deploy_payload = deploy_code + deploy_arguments
+          puts "sending payload #{deploy_payload}"
+          deploytx = connection.send_transaction({from: self.sender, gas: 2000000, gasPrice: 60000000000, data: deploy_payload})["result"]
+          instance_variable_set("@deployment", Ethereum::Deployment.new(deploytx, connection))
         end
 
         define_method :deployment do
-          self.instance_variable_get("@deployment")
+          instance_variable_get("@deployment")
         end
 
         define_method :deploy_and_wait do |time = 60.seconds, *params|
           self.deploy(*params)
           self.deployment.wait_for_deployment(time)
-          self.instance_variable_set("@address", self.deployment.contract_address)
+          instance_variable_set("@address", self.deployment.contract_address)
         end
 
         define_method :at do |addr|
-          self.instance_variable_set("@address", addr) 
+          instance_variable_set("@address", addr) 
         end
 
         define_method :address do
-          self.instance_variable_get("@address")
+          instance_variable_get("@address")
         end
 
         define_method :as do |addr|
-          self.instance_variable_set("@sender", addr)
+          instance_variable_set("@sender", addr)
         end
 
         define_method :sender do
-          self.instance_variable_get("@sender") || connection.coinbase["result"]
+          instance_variable_get("@sender") || connection.coinbase["result"]
         end
 
         define_method :set_gas_price do |gp|
-          self.instance_variable_set("@gas_price", gp)
+          instance_variable_set("@gas_price", gp)
         end
 
         define_method :gas_price do
-          self.instance_variable_get("@gas_price") || 60000000000
+          instance_variable_get("@gas_price") || 60000000000
         end
 
         define_method :set_gas do |gas|
-          self.instance_variable_set("@gas", gas)
+          instance_variable_set("@gas", gas)
         end
 
         define_method :gas do 
-          self.instance_variable_get("@gas") || 2000000
+          instance_variable_get("@gas") || 2000000
         end
 
         functions.each do |fun|
@@ -152,7 +156,10 @@ module Ethereum
 
         end
       end
-      Object.const_set(class_name, class_methods) unless Object.const_defined?(class_name)
+      if Object.const_defined?(class_name)
+        Object.send(:remove_const, class_name)
+      end
+      Object.const_set(class_name, class_methods)
     end
 
   end
