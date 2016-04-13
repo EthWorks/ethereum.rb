ee16604fece436ffd9680d388f447fb849fb0293
Anthony Eufemio <tymat@pris.im>
2016-04-12 22:39:40 -0700

    return nil if payload is nil

--- lib/ethereum/formatter.rb
+++ lib/ethereum/formatter.rb
@@ -37,42 +37,52 @@ module Ethereum
     end
 
     def from_bool(boolval)
+      return nil if boolval.nil?
       boolval ? "1" : "0"
     end
 
     def to_bool(hexstring)
+      return nil if hexstring.nil?
       (hexstring == "0000000000000000000000000000000000000000000000000000000000000001")
     end
 
     def to_ascii(hexstring)
+      return nil if hexstring.nil?
       hexstring.gsub(/^0x/,'').scan(/.{2}/).collect {|x| x.hex}.pack("c*")
     end
     
     def to_utf8(hexstring)
+      return nil if hexstring.nil?
       hexstring.gsub(/^0x/,'').scan(/.{2}/).collect {|x| x.hex}.pack("U*").delete("\u0000")
     end
 
     def from_ascii(ascii_string)
+      return nil if ascii_string.nil?
       ascii_string.unpack('H*')[0]
     end
 
     def from_utf8(utf8_string)
+      return nil if utf8_string.nil?
       utf8_string.force_encoding('UTF-8').split("").collect {|x| x.ord.to_s(16)}.join("")
     end
 
     def to_address(hexstring)
+      return "0x0000000000000000000000000000000000000000" if hexstring.nil?
       "0x" + hexstring[-40..-1]
     end
 
-    def to_wei(amount, unit)
+    def to_wei(amount, unit = "ether")
+      return nil if amount.nil?
       BigDecimal.new(UNITS[unit.to_sym] * amount, 16).to_s.to_i rescue nil
     end
 
-    def from_wei(amount, unit)
+    def from_wei(amount, unit = "ether")
+      return nil if amount.nil?
       (BigDecimal.new(amount, 16) / BigDecimal.new(UNITS[unit.to_sym], 16)).to_s rescue nil
     end
 
     def from_address(address)
+      return "0x0000000000000000000000000000000000000000" if address.nil?
       address.gsub(/^0x/,'').rjust(64, "0")
     end
 
@@ -89,6 +99,7 @@ module Ethereum
     end
 
     def to_int(hexstring)
+      return nil if hexstring.nil?
       (hexstring.gsub(/^0x/,'')[0..1] == "ff") ? (hexstring.hex - (2 ** 256)) : hexstring.hex
     end
 
@@ -150,3 +161,4 @@ module Ethereum
   end
 
 end
+
--- lib/ethereum/version.rb
+++ lib/ethereum/version.rb
@@ -1,3 +1,3 @@
 module Ethereum
-  VERSION = "0.4.98"
+  VERSION = "0.4.99"
 end
