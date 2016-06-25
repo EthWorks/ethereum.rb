b4e2cca5688b5085f1f3c17ad1ede5d3be612b7d
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2016-06-25 12:26:41 +0300

    add management api commands, update rpc commands. also, dont drop the namespace prefix from the method_name.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,14 +4,14 @@ describe Ethereum do
   describe 'HttpClient' do
     it 'should work' do
       client = Ethereum::HttpClient.new('localhost', '8545')
-      expect(client.accounts['error']).to eq(nil)
+      expect(client.eth_accounts['error']).to eq(nil)
     end
   end
 
   describe 'IpcClient' do
     it 'should work' do
       client = Ethereum::IpcClient.new("#{ENV['HOME']}/EtherDev/data/geth.ipc")
-      expect(client.accounts['error']).to eq(nil)
+      expect(client.eth_accounts['error']).to eq(nil)
     end
   end
 end
