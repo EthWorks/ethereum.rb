62fb2e68eda9346c9eeae30eab3c6797b798f752
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2016-06-25 12:51:13 +0300

    add additional safety checks around rpc id

--- lib/ethereum/client.rb
+++ lib/ethereum/client.rb
@@ -1,5 +1,6 @@
 module Ethereum
   class Client
+    class RpcIdMissmatchError < StandardError; end
 
     # https://github.com/ethereum/wiki/wiki/JSON-RPC
     RPC_COMMANDS = %w(web3_clientVersion web3_sha3 net_version net_peerCount net_listening eth_protocolVersion eth_syncing eth_coinbase eth_mining eth_hashrate eth_gasPrice eth_accounts eth_blockNumber eth_getBalance eth_getStorageAt eth_getTransactionCount eth_getBlockTransactionCountByHash eth_getBlockTransactionCountByNumber eth_getUncleCountByBlockHash eth_getUncleCountByBlockNumber eth_getCode eth_sign eth_sendTransaction eth_sendRawTransaction eth_call eth_estimateGas eth_getBlockByHash eth_getBlockByNumber eth_getTransactionByHash eth_getTransactionByBlockHashAndIndex eth_getTransactionByBlockNumberAndIndex eth_getTransactionReceipt eth_getUncleByBlockHashAndIndex eth_getUncleByBlockNumberAndIndex eth_getCompilers eth_compileLLL eth_compileSolidity eth_compileSerpent eth_newFilter eth_newBlockFilter eth_newPendingTransactionFilter eth_uninstallFilter eth_getFilterChanges eth_getFilterLogs eth_getLogs eth_getWork eth_submitWork eth_submitHashrate db_putString db_getString db_putHex db_getHex shh_post shh_version shh_newIdentity shh_hasIdentity shh_newGroup shh_addToGroup shh_newFilter shh_uninstallFilter shh_getFilterChanges shh_getMessages)
@@ -18,7 +19,7 @@ module Ethereum
     end
 
     def get_id
-      @id = @id + 1
+      @id = rand(9999999999) + 1
       return @id
     end
 
@@ -33,8 +34,12 @@ module Ethereum
         if @log == true
           @logger.info("Sending #{payload}")
         end
+
         read = send_single(payload)
         output = JSON.parse(read)
+
+        raise RpcIdMissmatchError unless output['id'] == @id+1
+
         return output
       end
     end
