5537b7d6b5943f3c8a1f7e63e324dc93d6dad258
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2016-06-26 13:42:38 +0300

    add a note

--- lib/ethereum/ipc_client.rb
+++ lib/ethereum/ipc_client.rb
@@ -16,6 +16,13 @@ module Ethereum
       return read
     end
 
+    # TODO: Not sure if multithread safe
+    # Note: Guarantees the results are in the same order as defined in batch call.
+    # client.batch do
+    #   client.eth_block_number
+    #   client.eth_mining
+    # end
+    # => [{"jsonrpc"=>"2.0", "id"=>1, "result"=>"0x26"}, {"jsonrpc"=>"2.0", "id"=>2, "result"=>false}] 
     def send_batch(batch)
       result = send_single(batch.to_json)
       result = JSON.parse(result)
