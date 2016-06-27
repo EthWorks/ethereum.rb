e1144f7d0f8aa6eb5f95734e3d0acb37e57227c3
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2016-06-27 23:48:52 +0300

    remove missmatch error

--- lib/ethereum/client.rb
+++ lib/ethereum/client.rb
@@ -1,7 +1,5 @@
 module Ethereum
   class Client
-    class RpcIdMissmatchError < StandardError; end
-
     # https://github.com/ethereum/wiki/wiki/JSON-RPC
     RPC_COMMANDS = %w(web3_clientVersion web3_sha3 net_version net_peerCount net_listening eth_protocolVersion eth_syncing eth_coinbase eth_mining eth_hashrate eth_gasPrice eth_accounts eth_blockNumber eth_getBalance eth_getStorageAt eth_getTransactionCount eth_getBlockTransactionCountByHash eth_getBlockTransactionCountByNumber eth_getUncleCountByBlockHash eth_getUncleCountByBlockNumber eth_getCode eth_sign eth_sendTransaction eth_sendRawTransaction eth_call eth_estimateGas eth_getBlockByHash eth_getBlockByNumber eth_getTransactionByHash eth_getTransactionByBlockHashAndIndex eth_getTransactionByBlockNumberAndIndex eth_getTransactionReceipt eth_getUncleByBlockHashAndIndex eth_getUncleByBlockNumberAndIndex eth_getCompilers eth_compileLLL eth_compileSolidity eth_compileSerpent eth_newFilter eth_newBlockFilter eth_newPendingTransactionFilter eth_uninstallFilter eth_getFilterChanges eth_getFilterLogs eth_getLogs eth_getWork eth_submitWork eth_submitHashrate db_putString db_getString db_putHex db_getHex shh_post shh_version shh_newIdentity shh_hasIdentity shh_newGroup shh_addToGroup shh_newFilter shh_uninstallFilter shh_getFilterChanges shh_getMessages)
     # https://github.com/ethereum/go-ethereum/wiki/Management-APIs
@@ -58,7 +56,6 @@ module Ethereum
         else
           read = send_single(payload.to_json)
           output = JSON.parse(read)
-          raise RpcIdMissmatchError unless output['id'] == @id
           reset_id
           return output
         end
