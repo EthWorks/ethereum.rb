ee61d8cf297456d53f83e07ac2804d9e2f4d12d2
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2016-06-28 00:15:51 +0300

    add client batching specs

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -9,9 +9,24 @@ describe Ethereum do
   end
 
   describe 'IpcClient' do
+    before(:each) do
+      @client = Ethereum::IpcClient.new("#{ENV['HOME']}/EtherDev/data/geth.ipc")
+    end
+
     it 'should work' do
-      client = Ethereum::IpcClient.new("#{ENV['HOME']}/EtherDev/data/geth.ipc")
-      expect(client.eth_accounts['error']).to eq(nil)
+      expect(@client.eth_accounts['error']).to eq(nil)
+    end
+
+    it 'should support batching' do
+      response = @client.batch do
+        @client.net_listening
+        @client.eth_block_number
+      end
+
+      expect(response).to be_a Array
+      expect(response.length).to eq 2
+      expect(response.first['result']).to be_in [true, false]
+      expect(response.last['result']).to match /(0x)?[0-9a-f]+/i
     end
   end
 end
