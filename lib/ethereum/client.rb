36c78cefda5f0f3102295e0fcee5213dd14f3a73
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2016-07-09 12:06:53 +0300

    improve Client.create

--- lib/ethereum/client.rb
+++ lib/ethereum/client.rb
@@ -18,8 +18,8 @@ module Ethereum
     end
 
     def self.create(host_or_ipcpath, log = false)
-      return IpcClient.new(host_or_ipcpath) if host_or_ipcpath.include? '.ipc'
-      return HttpClient.new(host_or_ipcpath) if host_or_ipcpath.include? 'http'
+      return IpcClient.new(host_or_ipcpath, log) if host_or_ipcpath.end_with? '.ipc'
+      return HttpClient.new(host_or_ipcpath, log) if host_or_ipcpath.start_with? 'http'
       raise ArgumentError.new('Unable to detect client type')
     end
 
