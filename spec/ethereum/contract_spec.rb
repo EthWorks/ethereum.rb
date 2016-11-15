ef72d8dc30b443e9d97f7e163c5b0dc5eda7b20e
Marek <marek.kirejczyk@gmail.com>
2016-11-15 08:55:34 +0100

    remove unnecessary line

--- spec/ethereum/contract_spec.rb
+++ spec/ethereum/contract_spec.rb
@@ -10,7 +10,6 @@ describe Ethereum do
     @init = Ethereum::Initializer.new("#{Dir.pwd}/spec/fixtures/Works.sol", @client)
     @init.build_all
     @works = Works.new
-    @works.set_gas 53000
     @works.deploy_and_wait
     @works.transact_and_wait_set("some4key", "somethevalue")
     expect(@works.call_get("some4key")).to eq "somethevalue"
