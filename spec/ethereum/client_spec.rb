aabe9297d9084dbd22c751d6a5c1d531cf22ef3b
Marek <marek.kirejczyk@gmail.com>
2017-01-15 16:07:48 +0100

    refactor client spec

--- spec/ethereum/client_spec.rb
+++ spec/ethereum/client_spec.rb
@@ -1,39 +1,52 @@
 require 'spec_helper'
 
-describe Ethereum do
+describe Ethereum::Client do
 
-  describe 'Client' do
+    let (:client) { Ethereum::Client.new }
 
-    before :all do
-      @client = Ethereum::Client.new
-      @result = '{"id":1, "jsonrpc":"2.0", "result": ""}'
+    describe '.encode_params' do
+      let (:params) { [true, false, 0, 12345, '0x7d84abf0f241b10927b567bd636d95fa9f66ae34', '0x4d5e07d4057dd0c3849c2295d20ee1778fc29d69150e8d75a07207347dce17fa', '0x7d84abf0f241b10927b567bd636d95fa9f66ae34'] }
+      let (:expected) { [true, false, '0x0', '0x3039', '0x7d84abf0f241b10927b567bd636d95fa9f66ae34', '0x4d5e07d4057dd0c3849c2295d20ee1778fc29d69150e8d75a07207347dce17fa', '0x7d84abf0f241b10927b567bd636d95fa9f66ae34'] }
+      subject { client.encode_params(params) }
+      it { is_expected.to eq expected}
     end
 
-    it 'should encode parameters' do
-      params = [true, false, 0, 12345, '0x7d84abf0f241b10927b567bd636d95fa9f66ae34', '0x4d5e07d4057dd0c3849c2295d20ee1778fc29d69150e8d75a07207347dce17fa', '0x7d84abf0f241b10927b567bd636d95fa9f66ae34']
-      expected = [true, false, '0x0', '0x3039', '0x7d84abf0f241b10927b567bd636d95fa9f66ae34', '0x4d5e07d4057dd0c3849c2295d20ee1778fc29d69150e8d75a07207347dce17fa', '0x7d84abf0f241b10927b567bd636d95fa9f66ae34']
-      encoded_params = @client.encode_params(params)
-      expect(encoded_params).to eq(expected)
-    end
-
-    def expect_to_send(expected)
-      expect(@client).to receive(:send_single).with(expected.to_json).and_return('{"id":1, "jsonrpc":"2.0", "result": ""}')
-      yield
-    end
-
-    it 'call json rpc methods' do
-      expect_to_send({"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":1}) { @client.web3_client_version }
-      expect_to_send({"jsonrpc":"2.0","method":"net_version","params":[],"id":1}) { @client.net_version }
-      expect_to_send({"jsonrpc":"2.0","method":"eth_protocolVersion","params":[],"id":1}) { @client.eth_protocol_version }
-      expect_to_send({"jsonrpc":"2.0","method":"eth_call","params":[{to: "0x407d73d8a49eeb85d32cf465507dd71d507100c1"}, "latest"], "id":1}) do
-        @client.eth_call(to: "0x407d73d8a49eeb85d32cf465507dd71d507100c1") 
+    shared_examples "json rpc method" do |request|
+      let (:response) { '{"id":1, "jsonrpc":"2.0", "result": ""}' }
+      it "is called" do
+        expect(client).to receive(:send_single).once.with(request.to_json).and_return(response)
+        subject
       end
-      expect_to_send({"jsonrpc":"2.0","method":"eth_getBalance","params":["0x407d73d8a49eeb85d32cf465507dd71d507100c1", "latest"],"id":1}) do
-        @client.eth_get_balance("0x407d73d8a49eeb85d32cf465507dd71d507100c1") 
-      end
-      expect_to_send({"jsonrpc":"2.0","method":"shh_newIdentity","params":[],"id":1}) { @client.shh_new_identity  }
     end
 
-  end
+    describe ".web3_client_version" do
+      subject { client.web3_client_version }
+      it_behaves_like "json rpc method", {"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":1}
+    end
+
+    describe ".net_version" do
+      subject { client.net_version }
+      it_behaves_like "json rpc method", {"jsonrpc":"2.0","method":"net_version","params":[],"id":1}
+    end
+
+    describe ".eth_protocol_version" do
+      subject { client.eth_protocol_version }
+      it_behaves_like "json rpc method", {"jsonrpc":"2.0","method":"eth_protocolVersion","params":[],"id":1}
+    end
+
+    describe ".shh_new_identity" do
+      subject { client.shh_new_identity }
+      it_behaves_like "json rpc method", {"jsonrpc":"2.0","method":"shh_newIdentity","params":[],"id":1}
+    end
+
+    describe ".eth_call" do
+      subject { client.eth_call(to: "0x407d73d8a49eeb85d32cf465507dd71d507100c1") }
+      it_behaves_like "json rpc method", {"jsonrpc":"2.0","method":"eth_call","params":[{to: "0x407d73d8a49eeb85d32cf465507dd71d507100c1"}, "latest"], "id":1}
+    end
+
+    describe ".eth_get_balance" do
+      subject { client.eth_get_balance("0x407d73d8a49eeb85d32cf465507dd71d507100c1") }
+      it_behaves_like "json rpc method", {"jsonrpc":"2.0","method":"eth_getBalance","params":["0x407d73d8a49eeb85d32cf465507dd71d507100c1", "latest"],"id":1}
+    end
 
 end
