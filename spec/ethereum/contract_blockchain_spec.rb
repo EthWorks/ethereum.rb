07cc725394a3756da90d00d88ee910b089781655
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2017-01-25 18:10:32 +0100

    blockchain test - give time for event to propagate

--- spec/ethereum/contract_blockchain_spec.rb
+++ spec/ethereum/contract_blockchain_spec.rb
@@ -12,6 +12,7 @@ describe Ethereum do
 
     filter_id = @works.new_filter.changed address: contract_address, topics: []
     tx_address = @works.transact_and_wait.set("some4key", "somethevalue").address
+    sleep 3
     expect(@works.get_filter_changes.changed(filter_id)[0][:transactionHash]).to eq tx_address
     expect(@works.get_filter_logs.changed(filter_id)[0][:transactionHash]).to eq tx_address
     expect(Ethereum::Transaction.from_blockchain(tx_address).mined?).to be true
