d0ef5d3b9fffe9e96b010c36b462a8d68ac4fc01
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-09-15 09:30:37 +0200

    update solidity spec to match new output version

--- spec/ethereum/solidity_spec.rb
+++ spec/ethereum/solidity_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Ethereum::Solidity do
   let (:greeter_bin) { '6060604052341561000f57600080fd5b604' }
   let (:mortal_bin) { '6060604052341561000f57600080fd5b5b6' }
-  let (:mortal_abi) { '[{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]' }
-  let (:greeter_abi) { '[{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"inputs":[{"name":"_greeting","type":"string"}],"payable":false,"type":"constructor"}]' }
+  let (:mortal_abi) { '[{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]' }
+  let (:greeter_abi) { '[{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_greeting","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]' }
   let (:contract_path) { "#{Dir.pwd}/spec/fixtures/greeter.sol" }
   let (:compiler_instance) { Ethereum::Solidity.new }
   let (:subject) { compiler_instance.compile(contract_path) }
