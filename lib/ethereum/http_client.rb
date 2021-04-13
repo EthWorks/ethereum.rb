8f360dbd9b85fe74b3dd6b57ad5f9ed75868d811
Cristi Stefan <cristian.stefan@xivic.com>
2021-04-13 13:57:09 +0300

    add basci auth as client optional params

--- lib/ethereum/http_client.rb
+++ lib/ethereum/http_client.rb
@@ -2,15 +2,16 @@ require 'net/http'
 require 'json'
 module Ethereum
   class HttpClient < Client
-    attr_accessor :host, :port, :uri, :ssl, :proxy
+    attr_accessor :host, :port, :uri, :ssl, :proxy, :options
 
-    def initialize(host, proxy = nil, log = false)
+    def initialize(host, proxy = nil, log = false, **options)
       super(log)
       uri = URI.parse(host)
       raise ArgumentError unless ['http', 'https'].include? uri.scheme
       @host = uri.host
       @port = uri.port
       @proxy = proxy
+      @options = options
       
       @ssl = uri.scheme == 'https'
       @uri = URI("#{uri.scheme}://#{@host}:#{@port}#{uri.path}")
@@ -29,6 +30,9 @@ module Ethereum
       end
       header = {'Content-Type' => 'application/json'}
       request = ::Net::HTTP::Post.new(uri, header)
+      if options[:basic_auth].present?
+        request.basic_auth options[:basic_auth][:username], options[:basic_auth][:password]
+      end
       request.body = payload
       response = http.request(request)
       response.body
